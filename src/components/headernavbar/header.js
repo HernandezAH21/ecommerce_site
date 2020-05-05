1c2427cbfb40ab8d52f0da9889cd839174ed4086
Alan Hernandez <serrato.alan21@gmail.com>
2020-05-05 10:56:37 -0500

    finished navbar feature

--- src/components/headernavbar/header.js
+++ src/components/headernavbar/header.js
@@ -3,10 +3,10 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
 class Header extends Component {
+
     render() {
         return (
             <div className='header'>
-                <img src='http://via.placeholder.com/50x50'/>
                 <img className='header__img' src='http://via.placeholder.com/50x50'/>
                 <div className='header__links'>
                 {
