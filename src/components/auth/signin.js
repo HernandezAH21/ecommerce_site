bf17011a82d84858f27beff6be526a358b22ae3e
Alan Hernandez <serrato.alan21@gmail.com>
2020-05-05 11:14:00 -0500

    finished header feature

--- src/components/auth/signin.js
+++ src/components/auth/signin.js
@@ -3,19 +3,30 @@ import React, { Component } from 'react';
 import SignInForm from './signinForm';
 import PageTitle from '../pageTitle';
 
+import { connect } from 'react-redux';
+import * as actions from '../../actions';
+
 class SignIn extends Component {
+
+    componentDidMount() {
+        this.props.setHeaderLinks([]);
+        this.props.setNavbarLinks([]);
+    }
 
     onSubmit = (fields) => {
         console.log(fields);
     }
-
+    
     render() {
         return (
             <div className='sign-in'>
-            <PageTitle className='sign-in__page-title' title='Login' />
-            <SignInForm onSubmit={this.onSubmit} className='sign-in__form' />            </div>
+                <PageTitle className='sign-in__page-title' title='Login' />
+                <SignInForm onSubmit={this.onSubmit} className='sign-in__form' />
+            </div>
         )
     }
 }
 
-export default SignIn;
+SignIn = connect(null, actions)(SignIn);
+
+export default SignIn; 
